4960b9a04b17189f053af41ec9a274e08da7ae5c
pathumck <ckpathumck@gmail.com>
2024-10-14 23:30:31 +0530

    create model mapping

---
+++ src/main/java/lk/ijse/spring/util/Mapping.java
@@ -0,0 +1,17 @@
+package lk.ijse.spring.util;
+
+import lk.ijse.spring.dto.impl.ItemDTO;
+import lk.ijse.spring.entity.impl.ItemEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mapping {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public ItemEntity toItemEntity(ItemDTO itemDTO){
+        return modelMapper.map(itemDTO, ItemEntity.class);
+    }
+}
