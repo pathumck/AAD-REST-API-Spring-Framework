66f9868934def331f0374dae2ea7ecb7c42a500e
pathumck <ckpathumck@gmail.com>
2024-10-17 14:06:37 +0530

    add loggers to HealthCheckController

--- src/main/java/lk/ijse/spring/controller/HealthCheckController.java
+++ src/main/java/lk/ijse/spring/controller/HealthCheckController.java
@@ -1,5 +1,7 @@
 package lk.ijse.spring.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/health")
 public class HealthCheckController {
+    private static final Logger logger = LoggerFactory.getLogger(HealthCheckController.class);
     @GetMapping
     public String healthTest() {
+        logger.info("Health check endpoint was called");
         return "Health controller is working";
     }
 }
