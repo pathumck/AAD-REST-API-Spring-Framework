0592b686c1dd58a7e3b8a0ff1ff3e0b955d3cf0e
pathumck <ckpathumck@gmail.com>
2024-10-17 14:22:00 +0530

    add loggers to ItemController

--- src/main/java/lk/ijse/spring/controller/ItemController.java
+++ src/main/java/lk/ijse/spring/controller/ItemController.java
@@ -7,6 +7,8 @@ import lk.ijse.spring.exception.DataPersistException;
 import lk.ijse.spring.exception.ItemNotFoundException;
 import lk.ijse.spring.service.ItemService;
 import lk.ijse.spring.util.RegexProcess;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,18 +20,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/item")
 public class ItemController {
+    private static final Logger logger = LoggerFactory.getLogger(ItemController.class);
     @Autowired
     private ItemService itemService;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> saveItem(@RequestBody ItemDTO itemDTO) {
         try {
+            logger.info("Saving item: {}", itemDTO);
             itemService.saveItem(itemDTO);
+            logger.info("Item saved: {}", itemDTO);
             return new ResponseEntity<>(HttpStatus.CREATED);
         }catch (DataPersistException exception) {
+            logger.error("Error while saving item: {}", itemDTO, exception);
             exception.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception exception) {
+            logger.error("Unexpected error while saving item: {}", itemDTO, exception);
             exception.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -39,14 +46,19 @@ public class ItemController {
     public ResponseEntity<Void> updateItem(@PathVariable ("itemId") String itemId, @RequestBody ItemDTO itemDTO) {
         try {
             if (!RegexProcess.itemIdValidation(itemId) || itemDTO == null) {
+                logger.warn("Invalid itemId or itemDTO: {}", itemId, itemDTO);
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
+            logger.info("Updating item with id: {}", itemId);
             itemService.updateItem(itemId, itemDTO);
+            logger.info("Item updated successfully: {}", itemDTO);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }catch (ItemNotFoundException exception) {
+            logger.error("Item not found: {}", itemId, exception);
             exception.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception exception) {
+            logger.error("Unexpected error while updating item: {}", itemDTO, exception);
             exception.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -56,14 +68,19 @@ public class ItemController {
     public ResponseEntity<Void> deleteItem(@PathVariable ("itemId") String itemId) {
         try {
             if (!RegexProcess.itemIdValidation(itemId)) {
+                logger.warn("Invalid itemId: {}", itemId);
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
+            logger.info("Deleting item with id: {}", itemId);
             itemService.deleteItem(itemId);
+            logger.info("Item deleted successfully: {}", itemId);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }catch (ItemNotFoundException exception) {
+            logger.error("Item not found: {}", itemId, exception);
             exception.printStackTrace();
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }catch (Exception exception) {
+            logger.error("Unexpected error while deleting item: {}", itemId, exception);
             exception.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -72,13 +89,16 @@ public class ItemController {
     @GetMapping(value = "/{itemId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ItemStatus getSelectedItem(@PathVariable("itemId") String itemId) {
         if (!RegexProcess.itemIdValidation(itemId)) {
+            logger.warn("Invalid item id: {}", itemId);
             return new SelectedItemAndCustomerErrorStatus(1, "Invalid Item Id");
         }
+        logger.info("Retrieving selected item with id: {}", itemId);
         return itemService.getSelectedItem(itemId);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<ItemDTO> getAllItems() {
+        logger.info("Retrieving all items");
         return itemService.getAllItems();
     }
 
